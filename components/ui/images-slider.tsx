66c0051b33ecb10ea86239e6f12efbf93fee5ee6
AnkitSoni03 <120096797+AnkitSoni03@users.noreply.github.com>
2025-09-24 10:32:26 +0530

    #

--- components/ui/images-slider.tsx
+++ components/ui/images-slider.tsx
@@ -85,6 +85,37 @@ export const ImagesSlider = ({
     };
   }, []);
 
+  // const slideVariants = {
+  //   initial: {
+  //     scale: 0,
+  //     opacity: 0,
+  //     rotateX: 45,
+  //   },
+  //   visible: {
+  //     scale: 1,
+  //     rotateX: 0,
+  //     opacity: 1,
+  //     transition: {
+  //       duration: 0.5,
+  //       ease: [0.645, 0.045, 0.355, 1.0],
+  //     },
+  //   },
+  //   upExit: {
+  //     opacity: 1,
+  //     y: "-150%",
+  //     transition: {
+  //       duration: 1,
+  //     },
+  //   },
+  //   downExit: {
+  //     opacity: 1,
+  //     y: "150%",
+  //     transition: {
+  //       duration: 1,
+  //     },
+  //   },
+  // };
+
   const slideVariants = {
     initial: {
       scale: 0,
@@ -100,16 +131,9 @@ export const ImagesSlider = ({
         ease: [0.645, 0.045, 0.355, 1.0],
       },
     },
-    upExit: {
+    exit: {
       opacity: 1,
-      y: "-150%",
-      transition: {
-        duration: 1,
-      },
-    },
-    downExit: {
-      opacity: 1,
-      y: "150%",
+      y: direction === "up" ? "-150%" : "150%",
       transition: {
         duration: 1,
       },
@@ -137,12 +161,22 @@ export const ImagesSlider = ({
 
       {areImagesLoaded && (
         <AnimatePresence>
-          <motion.img
+          {/* <motion.img
             key={currentIndex}
             src={loadedImages[currentIndex]}
             initial="initial"
             animate="visible"
             exit={direction === "up" ? "upExit" : "downExit"}
+            variants={slideVariants}
+            className="image h-full w-full absolute inset-0 object-cover object-center"
+          /> */}
+
+          <motion.img
+            key={currentIndex}
+            src={loadedImages[currentIndex]}
+            initial="initial"
+            animate="visible"
+            exit="exit"
             variants={slideVariants}
             className="image h-full w-full absolute inset-0 object-cover object-center"
           />
