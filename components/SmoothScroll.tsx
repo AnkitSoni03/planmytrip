392b7a460075514d17892d4f6f63878daa075944
AnkitSoni03 <120096797+AnkitSoni03@users.noreply.github.com>
2025-10-14 15:19:26 +0530

    #

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,4 +1,34 @@
-"use client";
+// "use client";
+
+// import { useEffect } from "react";
+// import Lenis from "@studio-freight/lenis";
+
+// const SmoothScroll = () => {
+//   useEffect(() => {
+//     const lenis = new Lenis({
+//       duration: 1.2,
+//       easing: (t) => t,
+//       lerp: 0.1,
+//     });
+
+//     function raf(time: number) {
+//       lenis.raf(time);
+//       requestAnimationFrame(raf);
+//     }
+
+//     requestAnimationFrame(raf);
+
+//     return () => lenis.destroy();
+//   }, []);
+
+//   return null;
+// };
+
+// export default SmoothScroll;
+
+
+
+"use client"; // Next.js app router me client component ke liye
 
 import { useEffect } from "react";
 import Lenis from "@studio-freight/lenis";
@@ -6,9 +36,10 @@ import Lenis from "@studio-freight/lenis";
 const SmoothScroll = () => {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => t,
-      lerp: 0.1,
+      duration: 1.2, // scroll duration
+      easing: (t) => t, // easing function
+      // @ts-ignore
+      smooth: true, // smooth scrolling enable
     });
 
     function raf(time: number) {
@@ -18,7 +49,7 @@ const SmoothScroll = () => {
 
     requestAnimationFrame(raf);
 
-    return () => lenis.destroy();
+    return () => lenis.destroy(); // cleanup on unmount
   }, []);
 
   return null;
